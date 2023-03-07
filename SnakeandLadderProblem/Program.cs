a96c521bc341a67aece7dbcc99b5d91560a1f521
Shrikant <sawantshrikant07@gmail.com>
2023-03-07 16:39:08 +0530

    [Shrikant] add:PlayerCheckOption

--- SnakeandLadderProblem/Program.cs
+++ SnakeandLadderProblem/Program.cs
@@ -8,8 +8,23 @@ class Program
 
         Play game = new Play();
         PlayerRollsTheDie playerRollsTheDie = new PlayerRollsTheDie();
+        CheckOption checkOption = new CheckOption();
 
         int roll = playerRollsTheDie.RollDie();
-        game.PlaySnakeAndLadder(roll);
+        Console.WriteLine("You rolled {0}.", roll);
+
+        int option = checkOption.CheckForOption();
+
+        switch (option)
+        {
+            case 0:
+                break;
+            case 1:
+                game.PlaySnakeAndLadder(roll);
+                break;
+            case 2:
+                game.PlaySnakeAndLadder(-roll);
+                break;
+        }
     }
 }
